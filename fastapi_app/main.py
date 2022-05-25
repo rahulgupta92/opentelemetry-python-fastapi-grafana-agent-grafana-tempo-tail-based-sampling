6d0e5163c0c99cc42f6fc368e40cf314cf1b0e45
Rahul Gupta <rgupta@ocrolus.com>
2022-05-25 15:39:08 +0530

    Add Black, isort and mypy to requirements.txt and apply black formatting

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -2,6 +2,7 @@ from typing import Union
 
 from fastapi import FastAPI
 from opentelemetry import trace
+
 # from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.resources import Resource
@@ -15,12 +16,10 @@ FastAPIInstrumentor.instrument_app(app)
 
 # Resource can be required for some backends, e.g. Jaeger
 # If resource wouldn't be set - traces wouldn't appears in Jaeger
-resource = Resource(attributes={
-    "service.name": "fastapi_server"
-})
+resource = Resource(attributes={"service.name": "fastapi_server"})
 
 trace.set_tracer_provider(TracerProvider(resource=resource))
-endpoint = 'http://agent:4318/v1/traces'
+endpoint = "http://agent:4318/v1/traces"
 
 otlp_exporter = OTLPSpanExporter(endpoint=endpoint)
 span_processor = BatchSpanProcessor(otlp_exporter)
@@ -28,17 +27,16 @@ trace.get_tracer_provider().add_span_processor(span_processor)
 tracer = trace.get_tracer(__name__)
 
 
-@app.get('/')
+@app.get("/")
 def index():
     with tracer.start_as_current_span("foo") as span:
         print(span.get_span_context())
         print(span.parent.trace_id)
         print(span.get_span_context().trace_id)
         print("Hello world!")
-    return {'Hello': 'World'}
+    return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
-
